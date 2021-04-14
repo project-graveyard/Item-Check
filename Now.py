9488e01403d80ec7b9559bedd29a31f0d6976845
David Saah <dasorange.hope@gmail.com>
2021-04-14 20:19:08 +0000

    A time module

--- Now.py
+++ Now.py
@@ -2,9 +2,10 @@ from datetime import datetime
 
 today = datetime.now()
 
+
 def timestamp():
     now = today.strftime("%B %d, %Y   %H:%M:%S")
     print('{0:>60}'.format(now))
-    
+
 
 curt = today.strftime("%d-%m-%Y_%H:%M:%S")
