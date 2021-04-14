39571f1a4626e60cfdc9a215b776590e4874b753
David Saah <dasorange.hope@gmail.com>
2021-04-14 20:19:49 +0000

    A module that allows clear screen

--- clear_screen.py
+++ clear_screen.py
@@ -2,6 +2,10 @@ import os
 
 # Clear function
 clf = 'clear'
-if os.name == 'posix': clf = 'clear'
-if os.name == 'nt': clf = 'cls'
-clear = lambda: os.system(clf)
+if os.name == 'posix':
+    clf = 'clear'
+if os.name == 'nt':
+    clf = 'cls'
+
+
+def clear(): return os.system(clf)
